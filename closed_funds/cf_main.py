a455e601a7fffa6da8f033f913cd6adbd206e7f0
wyh-mac <479400810@qq.com>
2021-05-07 23:41:23 +0800

    weight 1

--- closed_funds/cf_main.py
+++ closed_funds/cf_main.py
@@ -1,6 +1,5 @@
 from spider_data.CFunds import ClosedFunds
 import pandas as pd
-import datetime
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', None)
 pd.set_option("display.max_colwidth", 100)
@@ -8,8 +7,10 @@ cf = ClosedFunds()
 df = cf.get_cf()
 df['week1_nav_incr_rt'] = df['week1_nav_incr_rt'].str.strip('%').astype('float')
 df['month1_nav_incr_rt'] = df['month1_nav_incr_rt'].str.strip('%').astype('float')
+# 权重比例 weight 周和月比例设置 1：4
+weight = 1
 # 目前综合得分按照  week+month/4 计算
-df['score'] = df['week1_nav_incr_rt'] + df['month1_nav_incr_rt']/4
+df['score'] = df['week1_nav_incr_rt'] + df['month1_nav_incr_rt']/weight
 df['score'] = df['score'].apply(lambda x: round(x,2))
 columns = {'fund_id': '基金代码',
            'fund_nm': '基金名称',
@@ -22,6 +23,6 @@ columns = {'fund_id': '基金代码',
            'score': '综合得分'}
 df_score = df[columns.keys()].sort_values(by='score', ascending=False)
 df_score = df_score.rename(columns=columns)
-file_name = 'result/cf_{0}.csv'.format(str(datetime.datetime.now().date()))
+file_name = 'result/cf_1_{0}.csv'.format(weight)
 df_score.to_csv(file_name, index=False, encoding='utf_8_sig')
 
